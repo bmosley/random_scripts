e6a6e583995f5ac242f64989a7a4aac1c9a7d625
bmosley <bmosley@na>
2024-02-24 21:16:35 -0800

    Create local_weather.py

---
+++ python/local_weather.py
@@ -0,0 +1,63 @@
+import pprint
+import requests
+
+from datetime import datetime
+
+API_BASE = "http://api.openweathermap.org"
+API_KEY = ""
+
+
+def get_weather_data(lat=None, lon=None):
+    url = f"{API_BASE}/data/2.5/onecall?exclude=minutely,hourly,alerts&units=metric&lat={lat}&lon={lon}&appid={API_KEY}"
+    r = requests.get(url)
+    if not r.ok:
+        raise requests.ConnectionError("Did not get a valid response from the server.")
+
+    return r.json()
+
+
+def get_geo(city_name, state_code="CA", country_code="US", limit=3):
+    url = f"{API_BASE}/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit={limit}&appid={API_KEY}"
+    r = requests.get(url)
+    if not r.ok:
+        raise requests.ConnectionError("Did not get a valid response from the server.")
+
+    data = r.json()
+    if not data:
+        raise ValueError("No matching data returned from API call.")
+
+    matched_data = None
+
+    # this is long-hand `for-loop` so its easier to read.
+    # normally, we'd use a list comprehension instead to simplify.
+    for loc in data:
+        # if the city matches what we asked for _exactly_ we can stop looking and bail out.
+        if loc.get("name") == city_name:
+            matched_data = loc
+            break
+
+    if not matched_data:
+        raise ReferenceError(f"Cannot find a matched city for '{city_name}' in response query.")
+
+    lat = matched_data["lat"]
+    lon = matched_data["lon"]
+
+    return lat, lon
+
+
+lat, lon = get_geo("Campbell")
+data = get_weather_data(lat=lat, lon=lon)
+
+
+current_weather = data.get("current")
+forecast = data.get("daily")
+
+pprint.pprint(current_weather)
+print("**************************")
+for day in forecast:
+    # note this doesnt account for timezone
+    date = datetime.fromtimestamp(day.get("dt"))
+    print("------------------")
+    print(f"{date}")
+    print("------------------")
+    pprint.pprint(day)
